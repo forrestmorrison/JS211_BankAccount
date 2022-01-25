7f131b7609928a7fa66763efc486c5e08ed5264d
forrestmorrison <forrestmorrison@gmail.com>
2022-01-25 13:56:05 -0600

    functioning BankAccount & Transaction classes

---
+++ main.js
@@ -0,0 +1,61 @@
+// Bank Account
+
+
+
+// create two classes: BankAccount & Transactions
+
+// BankAccount has 3 fields: accountNumber, owner, transactions
+// the constructor function in BankAccount takes 2 inputs: accountNumber, owner 
+
+class BankAccount {
+    constructor (accountNumber, owner) {
+      this.accountNumber = accountNumber;
+      this.owner = owner;
+      this.transactions = [];
+    }
+
+    // BankAccount has 3 methods:
+    
+    // balance() - takes no input, returns current balance on account
+    // balance is computed by summing up amounts in transactions array
+    balance() {
+        let sum = 0;
+        for (let i = 0; i < this.transactions.length; i++) {
+            let element = this.transactions[i];
+            sum = sum + element.amount;
+        }
+        return sum;
+    };
+
+    // deposit(amt) - takes single input: the deposit amount, creates new transaction & adds it to transactions array
+    deposit(amt) {
+        let newDeposit = new Transaction (amt, this.owner);
+        this.transactions.push(newDeposit);
+    };
+
+    // charge(payee, amount) - takes 2 inputs: payee, amount creates new transaction with payee & amount, adds it to transaction array
+    charge(payee, amt) {
+        let newCharge = new Transaction (amt, payee);
+        this.transactions.push(newCharge);
+    };
+}
+
+// Transaction has 3 fields: date, amount, payee
+// the constructor function in Transaction takes 2 inputs: amount, payee
+
+class Transaction {
+    constructor (amount, payee) {
+        this.date = new Date();
+        this.amount = amount;
+        this.payee = payee;
+    }
+}
+
+let toddCarter = new BankAccount(001, "Todd Carter");
+
+toddCarter.deposit(55);
+toddCarter.deposit(35);
+toddCarter.charge("Chipotle", -20);
+
+console.log(toddCarter.balance());
+console.log(toddCarter.transactions);
